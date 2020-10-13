ea8a01edb69d03fe119f4eec9cc67fdc03aadbd5
李大平 <lidaping@graphicchina.com>
2020-10-13 17:36:52 +0800

    路由配置好了。

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Home msg="Welcome to Your Vue.js App"/>
+  <div id='root'>
+    <img src='./assets/logo.png' />
+    <div id='nav'>
+      <router-link to='/'> Home</router-link>
+      <router-link to='/contact'>Contact </router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
-
-<script>
-import Home from './pages/Home.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Home
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
