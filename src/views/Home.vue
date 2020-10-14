e03c129ac7de2c1e450b5a91ac4a3c33415b6ad9
leonMBP <leon.zju@qq.com>
2020-10-14 23:26:58 +0800

    ts version ok

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,103 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <div class="header">
+      <div class="header-content" :class="headerThemeClass">
+        <div class="logo">
+          <span>LOGO</span>
+        </div>
+        <ul class="header-menu">
+          <li class="header-menu-item">
+            <a href="/">Home</a>
+          </li>
+          <li class="header-menu-item">
+            <a href="/">Development</a>
+          </li>
+          <li class="header-menu-item">
+            <a href="/">Production</a>
+          </li>
+          <li class="header-menu-item">
+            <a href="/">Pricing</a>
+          </li>
+          <li class="header-menu-item">
+            <a href="/contact">About Us</a>
+          </li>
+          <li class="header-menu-item">
+            <a href="/contact">FAQ</a>
+          </li>
+          <li class="header-menu-item">
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <img class="top-big-img" src="../assets/big-img.jpg" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Options({
-  components: {
-    HelloWorld
-  }
+  components: {},
+  props: {}
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  headerThemeClass = "dark-theme";
+  mounted() {
+    //给window添加一个滚动滚动监听事件
+    window.addEventListener("scroll", this.handleScroll);
+  }
+  handleScroll() {
+    //获取滚动条位置
+    const scrollTop =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop;
+    if (scrollTop <= 5) {
+      this.headerThemeClass = "dark-theme";
+    } else {
+      this.headerThemeClass = "light-theme";
+    }
+  }
+  unmounted() {
+    //离开该页面需要移除这个监听的事件
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.header {
+  position: fixed;
+  width: 100%;
+}
+.dark-theme {
+  background-color: transparent;
+  color: white;
+}
+.light-theme {
+  background-color: white;
+  color: black;
+}
+.header-content {
+  position: static;
+  display: flex;
+  display: -webkit-flex;
+  justify-content: space-between;
+}
+.header-menu {
+  display: flex;
+  display: -webkit-flex;
+  justify-content: space-between;
+}
+.header-menu-item {
+  margin-left: 5px;
+  display: block;
+}
+.top-big-img {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  bottom: 0;
+}
+</style>
