2567b3a54ded2dcfc91032cd38329b7baa6d4a69
李大平 <lidaping@graphicchina.com>
2020-10-16 20:22:51 +0800

    初步完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -80,63 +80,60 @@
         </div>
         <a href="/contact" class="second-third-item-button">Contact Now</a>
       </div>
-      <div id="fourth-item" class="third-item-container">
-        <div class="third-item-text-container">
-          <div class="third-item-main">Production</div>
-          <div class="third-item-minor">
-            No matter whether you wish to develop your line with us or you have
-            already got your line developed, we work with you for your needs
-            with a fast turnaround. No matter whether you are a made-to-order
-            business, a small startup looking for ten pieces, or a larger brand
-            in need of producing thousands of pieces, we work with you. There is
-            no minimum order quantity requirement. We will refund basic sample
-            fee and offer free pattern development for any order of more than
-            one hundred production pieces.
-          </div>
-          <a href="/contact" class="second-third-item-button">Contact Now</a>
+    </div>
+    <div id="fourth-item" class="third-item-container">
+      <div class="third-item-text-container">
+        <div class="third-item-main">Production</div>
+        <div class="third-item-minor">
+          No matter whether you wish to develop your line with us or you have
+          already got your line developed, we work with you for your needs with
+          a fast turnaround. No matter whether you are a made-to-order business,
+          a small startup looking for ten pieces, or a larger brand in need of
+          producing thousands of pieces, we work with you. There is no minimum
+          order quantity requirement. We will refund basic sample fee and offer
+          free pattern development for any order of more than one hundred
+          production pieces.
         </div>
-        <img src="../assets/fourth-item.jpg" class="third-item-img" />
+        <a href="/contact" class="second-third-item-button">Contact Now</a>
       </div>
+      <img src="../assets/fourth-item.jpg" class="third-item-img" />
     </div>
     <div id="why-us" class="why-us-container">
+      <div class="why-us-text">Why us?</div>
       <div class="commitment-img">
         <div class="why-us-text-container">
-          <div class="third-item-main">Development</div>
+          <div class="third-item-main">Your brand, our commitment.</div>
           <div class="third-item-minor">
-            We provide the whole package of development. We start with listening
-            to you, understanding your concept and your customers better.
-            Together with you, we create patterns, do grading and make fit
-            samples for your approval. We can even source fabrics in Italy,
-            Japan, Korea, China from our large network of fabric vendors for
-            you. During the sampling stage, we will provide pictures of muslin
-            samples and revise samples for free before actual sample making. At
-            the end of this development process, you will get your
-            production-ready lines that you love.
+            At Seam Placement, we take pride in the techniques that our tailors
+            are skilled at. French seam, engineering, embroidery, you name it.
+            Every single one of our tailors has years of fashion production
+            experience and holds a certificate of tailoring technician. We
+            create quality clothes that last.
           </div>
-          <a href="/contact" class="second-third-item-button">Contact Now</a>
+          <a href="/contact" class="second-third-item-button"
+            >Get Free Consultation</a
+          >
         </div>
       </div>
       <div class="communication-img">
         <div class="why-us-text-container">
-          <div class="third-item-main">Development</div>
+          <div class="third-item-main">Communication is everything.</div>
           <div class="third-item-minor">
-            We provide the whole package of development. We start with listening
-            to you, understanding your concept and your customers better.
-            Together with you, we create patterns, do grading and make fit
-            samples for your approval. We can even source fabrics in Italy,
-            Japan, Korea, China from our large network of fabric vendors for
-            you. During the sampling stage, we will provide pictures of muslin
-            samples and revise samples for free before actual sample making. At
-            the end of this development process, you will get your
-            production-ready lines that you love.
+            We start by listening to your vision and the concept of your line.
+            We would like to understand your line and your customers better so
+            we can develop the clothes that fit them. We also keep in touch with
+            your in-house design and development team throughout the process to
+            make sure what we deliver fits your vision.
           </div>
-          <a href="/contact" class="second-third-item-button">Contact Now</a>
+          <a href="/contact" class="second-third-item-button"
+            >Get Free Consultation</a
+          >
         </div>
       </div>
     </div>
     <div id="testimonial" class="testimonial-container">
       <div class="third-item-main">Testimonial</div>
-      <div class="third-item-minor">
+      <div class="testimonial-item-minor">
         Having considered dozens of factories in China, we chose Seam Placement
         as our strategic partner. It is one of the best decisions that we have
         made. Seam Placement helped us launch our first customization line. They
@@ -153,43 +150,54 @@
       We love making clothes. We pay attention to every detail to make our
       products meet your expectation.
     </div>
+    <div class="contact-title">Contact</div>
     <div id="contact" class="contact-container">
-      <div class="contact-form-container">
-        <form class="contact-form">
-          <div class="send-us-a-message">
-            Send us a message.
-          </div>
-          <input type="text" class="contact-form-input-text" />
-          <input type="text" class="contact-form-input-text" />
-          <input type="text" class="contact-form-input-text" />
-          <input type="text" class="contact-form-input-text" />
-          <button class="submit">SUBMIT</button>
-        </form>
-      </div>
-      <div class="address-email-container">
-        <div class="address-item">
-          <img class="address-icon" src="../assets/address.png" />
-          <div class="address-text">
-            3rd Floor, Building No.17, Xinhua Group Creative Garden, No.59
-            Shixiang Road, Xiacheng District, Hangzhou, Zhejiang, China
-          </div>
+      <form class="contact-form">
+        <div class="send-us-a-message">
+          Send us a message.
         </div>
-        <div class="address-item">
-          <img class="address-icon" src="../assets/email.png" />
-          <div class="address-text">
-            jenny@seamplacement.com
-          </div>
-        </div>
-        <div class="address-item">
-          <img class="address-icon" src="../assets/skype.png" />
-          <div class="address-text">
-            Skype: seam placement
-          </div>
+        <input type="text" placeholder="Name" class="contact-form-input-text" />
+        <input
+          type="text"
+          placeholder="Email"
+          class="contact-form-input-text"
+        />
+        <input
+          type="text"
+          placeholder="Company(optional)"
+          class="contact-form-input-text"
+        />
+        <input
+          type="text"
+          placeholder="Leave a message."
+          class="contact-form-input-text"
+        />
+        <button class="submit">SUBMIT</button>
+      </form>
+    </div>
+    <div class="address-email-container">
+      <div class="address-item">
+        <img class="address-icon" src="../assets/address.png" />
+        <div class="address-text">
+          3rd Floor, Building No.17, Xinhua Group Creative Garden, No.59
+          Shixiang Road, Xiacheng District, Hangzhou, Zhejiang, China
         </div>
       </div>
-      <div class="copyright">
-        Copyright © 2019 Seamplacement. All rights reserved.
+      <div class="address-item">
+        <img class="address-icon" src="../assets/email.png" />
+        <div class="address-text">
+          jenny@seamplacement.com
+        </div>
       </div>
+      <div class="address-item">
+        <img class="address-icon" src="../assets/skype.png" />
+        <div class="address-text">
+          Skype: seam placement
+        </div>
+      </div>
+    </div>
+    <div class="copyright">
+      Copyright © 2019 Seamplacement. All rights reserved.
     </div>
   </div>
 </template>
@@ -399,5 +407,167 @@ export default class Home extends Vue {
     background-color: black;
     color: white;
   }
+}
+.third-item-container {
+  display: flex;
+}
+.third-item-img {
+  width: 45%;
+  margin: 5%;
+}
+.third-item-text-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 48%;
+  margin: 5% 2%;
+}
+.third-item-main {
+  font-size: 40px;
+  margin-bottom: 40px;
+}
+.third-item-minor {
+  font-size: 16px;
+  margin: 20px 0px 20px 0px;
+}
+.why-us-text {
+  font-size: 40px;
+  margin-bottom: 40px;
+  text-align: center;
+}
+.commitment-img {
+  background-image: url("../assets/fifth-item.jpg");
+  height: 150vh;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+}
+.why-us-text-container {
+  width: 40%;
+  display: flex;
+  background-color: darkgrey;
+  opacity: 0.8;
+  padding: 20px;
+  margin: 40px;
+  margin-bottom: 100px;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.communication-img {
+  background-image: url("../assets/sixth-item.jpg");
+  height: 150vh;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+}
+.testimonial-container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 100px;
+}
+.testimonial-item-minor {
+  font-size: 20px;
+  margin: 80px;
+}
+.guest-sign-container {
+  display: flex;
+  width: 90%;
+  justify-content: flex-end;
+  align-items: center;
+}
+.guest-sign {
+  margin-right: 20px;
+  font-size: 20px;
+}
+.sayings {
+  margin-top: 50px;
+  background-image: url("../assets/rectangle.png");
+  height: 200px;
+  font-size: 30px;
+  color: white;
+  text-align: center;
+  line-height: 200px;
+}
+.contact-title {
+  font-size: 40px;
+  margin-bottom: 40px;
+  margin-top: 40px;
+  text-align: center;
+}
+.contact-container {
+  margin-top: 50px;
+  background-image: url("../assets/map_location.png");
+  height: 700px;
+  font-size: 30px;
+  color: white;
+  text-align: center;
+  line-height: 200px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+}
+.contact-form {
+  margin-right: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  background-color: white;
+  padding: 20px;
+  border-radius: 4px;
+}
+.send-us-a-message {
+  font-size: 24px;
+  color: black;
+  line-height: 20px;
+  margin-bottom: 20px;
+}
+.contact-form-input-text {
+  border: 0px;
+  border-bottom: 2px solid darkgrey;
+  width: 300px;
+  height: 40px;
+  margin: 20px 20px;
+  font-size: 24px;
+  outline: none; // 去除选中状态边框
+}
+.submit {
+  color: white;
+  font-size: 24px;
+  text-align: center;
+  border: 1px solid black;
+  border-radius: 2px;
+  padding: 10px 0px;
+  margin: 30px 40px;
+  text-decoration-line: none;
+  background-color: black;
+  &:hover {
+    background-color: white;
+    color: black;
+  }
+}
+.address-email-container {
+  display: flex;
+  margin: 40px 5%;
+}
+.address-item {
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+}
+.address-icon {
+  margin: 10px;
+}
+.copyright {
+  font-size: 26px;
+  color: darkgray;
+  text-align: center;
+  margin: 40px 0px;
+  padding-top: 20px;
+  width: 100%;
+  border-top: 1px solid gray;
 }
 </style>
